2358beed84b8287d182c0b41e8742226c1bb3616
davidjm10 <134171710+davidjm10@users.noreply.github.com>
2024-08-08 07:55:37 -0500

    Update App.java

--- app/src/main/java/quiz1sistemas/App.java
+++ app/src/main/java/quiz1sistemas/App.java
@@ -3,8 +3,54 @@ package quiz1sistemas;
 
 public class App {
     public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+
+        int precioGeneral = 10000;
+        int precioVip = 15000;
+        int precioCombo1 = 40000;
+        int precioCombo2 = 30000;
+        int precioCombo3 = 42000;
+
+        try {
+            System.out.println("que tipo de boleta desea comprar (general/vip): ");
+            String tipoBoleta = scanner.nextLine();
+   
+            System.out.println("cuantas boletas desea comprar: ");
+            int cantidadBoletas = scanner.nextInt();
+            
+            System.out.println("que combo desea comprar (1,2,3): ");
+            int comboDeseado = scanner.nextInt();
+            
+            System.out.println("cuantos combos desea comparar: ");
+            int cantidadCombos = scanner.nextInt();
+
+            int valorBoletas = (tipoBoleta) = precioGeneral * cantidadBoletas;
+                return resultado;
+
+            
+            int valorCombo = 0;
+            switch (comboDeseado) {
+                case 1:
+                    valorCombo = precioCombo1 * cantidadCombos;
+                    break;
+                case 2:
+                    valorCombo = precioCombo2 * cantidadCombos;
+                    break;
+                case 3:
+                    valorCombo = precioCombo3 * cantidadCombos;
+                    break;
+                return ;
+            }
         
-        // Logica de solucion
+                int valorTotal = valorBoletas + valorCombo;
+
+                System.out.println("factura: ");
+                System.out.println("tipo de silla comprada" + tipoBoleta + );
+        
+        } catch (Exception e) {
+            System.out.println("El error es: " + e);     
+        }
         
     }
 
